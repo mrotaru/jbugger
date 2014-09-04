6fecc903d1a32a90ff15515741158f3dd705394b
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-09-04 23:32:25 +0100

    update browser name and version detection

--- jbugger.js
+++ jbugger.js
@@ -75,74 +75,44 @@ var ajax = function(options){
     }
 }
 
-// from: http://stackoverflow.com/a/11219680/447661
-function getBrowserInfo() {
-    var nVer = navigator.appVersion;
-    var nAgt = navigator.userAgent;
-    var browserName  = navigator.appName;
-    var fullVersion  = ''+parseFloat(navigator.appVersion); 
-    var majorVersion = parseInt(navigator.appVersion,10);
-    var nameOffset,verOffset,ix;
-
-    // In Opera, the true version is after "Opera" or after "Version"
-    if ((verOffset=nAgt.indexOf("Opera"))!=-1) {
-        browserName = "Opera";
-        fullVersion = nAgt.substring(verOffset+6);
-        if ((verOffset=nAgt.indexOf("Version"))!=-1) 
-            fullVersion = nAgt.substring(verOffset+8);
-    }
-    // In MSIE, the true version is after "MSIE" in userAgent
-    else if ((verOffset=nAgt.indexOf("MSIE"))!=-1) {
-        browserName = "Microsoft Internet Explorer";
-        fullVersion = nAgt.substring(verOffset+5);
-    }
-    // In Chrome, the true version is after "Chrome" 
-    else if ((verOffset=nAgt.indexOf("Chrome"))!=-1) {
-        browserName = "Chrome";
-        fullVersion = nAgt.substring(verOffset+7);
-    }
-    // In Safari, the true version is after "Safari" or after "Version" 
-    else if ((verOffset=nAgt.indexOf("Safari"))!=-1) {
-        browserName = "Safari";
-        fullVersion = nAgt.substring(verOffset+7);
-        if ((verOffset=nAgt.indexOf("Version"))!=-1) 
-            fullVersion = nAgt.substring(verOffset+8);
-    }
-    // In Firefox, the true version is after "Firefox" 
-    else if ((verOffset=nAgt.indexOf("Firefox"))!=-1) {
-        browserName = "Firefox";
-        fullVersion = nAgt.substring(verOffset+8);
-    }
-    // In most other browsers, "name/version" is at the end of userAgent 
-    else if ( (nameOffset=nAgt.lastIndexOf(' ')+1) < 
-             (verOffset=nAgt.lastIndexOf('/')) ) 
-         {
-             browserName = nAgt.substring(nameOffset,verOffset);
-             fullVersion = nAgt.substring(verOffset+1);
-             if (browserName.toLowerCase()==browserName.toUpperCase()) {
-                 browserName = navigator.appName;
-             }
-         }
-         // trim the fullVersion string at semicolon/space if present
-         if ((ix=fullVersion.indexOf(";"))!=-1)
-         fullVersion=fullVersion.substring(0,ix);
-         if ((ix=fullVersion.indexOf(" "))!=-1)
-             fullVersion=fullVersion.substring(0,ix);
-
-         majorVersion = parseInt(''+fullVersion,10);
-         if (isNaN(majorVersion)) {
-             fullVersion  = ''+parseFloat(navigator.appVersion); 
-             majorVersion = parseInt(navigator.appVersion,10);
-         }
-
-         return {
-             browserName: browserName,
-             browserVersion: fullVersion,
-             browserMajorVersion: majorVersion,
-             userAgent: navigator.userAgent
-         }
+// http://stackoverflow.com/a/16938481/447661
+function get_browser(){
+    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
+    if(/trident/i.test(M[1])){
+        tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
+        return 'IE '+(tem[1]||'');
+    }   
+    if(M[1]==='Chrome'){
+        tem=ua.match(/\bOPR\/(\d+)/)
+        if(tem!=null)   {return 'Opera '+tem[1];}
+    }   
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+    return M[0];
 }
 
+function get_browser_version(){
+    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];                                                                                                                         
+    if(/trident/i.test(M[1])){
+        tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1]||'');
+    }
+    if(M[1]==='Chrome'){
+        tem=ua.match(/\bOPR\/(\d+)/)
+        if(tem!=null)   {return 'Opera '+tem[1];}
+    }   
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+    return M[1];
+}
+
+function getBrowserInfo(){
+    return {
+        browserName: get_browser(),
+        browserVersion: get_browser_version(),
+        userAgent: navigator.userAgent
+    }
+}
 
 // http://stackoverflow.com/a/11744120/447661
 function getViewportInfo() {
